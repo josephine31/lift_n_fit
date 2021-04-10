cda766f00d5e93e3d62871597551c6563fb05de6
antony.george <antony.george@neosofttech.com>
2021-04-10 20:15:47 +0530

    project setup

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,12 @@
+export const firebaseconfig = {
+
+    firebase : {
+        apiKey: "AIzaSyBr6rCKz7-tklW3QL5URgt7GaX6rWf3Vuo",
+        authDomain: "AUTH_DOMAIN",
+        databaseURL: "DATABASE_URL",
+        projectId: "gym-manage-df3fc",
+        storageBucket: '',
+        messagingSenderId: ''
+      }
+
+};
